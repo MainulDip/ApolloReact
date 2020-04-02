6156214fb2f2f6792ab06591754436310d9539e8
Mainul Dip <mainuldip@gmail.com>
2020-04-02 22:21:42 +0600

    Server Created on GraphQL and Initial Commit before react apollo

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const graphHTTP = require('express-graphql')
+const schema = require('./schema.js')
+
+const app = express()
+
+app.use(
+  '/graphql',
+  graphHTTP({
+    schema,
+    graphiql: true
+  })
+)
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server started at port ${PORT}`))
